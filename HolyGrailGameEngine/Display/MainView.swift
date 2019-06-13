33e75eb7a25f9ed590003cd3b62d7099ca158ad7
Rick Twohy <twohyjr@gmail.com>
2019-06-13 06:13:51 -0400

    Project Setup
    Created Main View
    Resized Window
    Created Split Pane

---
+++ HolyGrailGameEngine/Display/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  HolyGrailGameEngine
+//
+//  Created by Rick Twohy Jr on 6/13/19.
+//  Copyright © 2019 Rick Twohy Jr. All rights reserved.
+//
+
+import MetalKit
+
+class MainView: MTKView {
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+}
+
