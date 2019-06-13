92f169d4023a100ddd1ce262eac48e82d531978b
Rick Twohy <twohyjr@gmail.com>
2019-06-13 06:06:28 -0400

    Initial Commit

---
+++ HolyGrailGameEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HolyGrailGameEngine
+//
+//  Created by Rick Twohy Jr on 6/13/19.
+//  Copyright © 2019 Rick Twohy Jr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
